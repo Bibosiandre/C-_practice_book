40711717b1c15777c6d3402262e56e0c184b177d
Vladislav Minibaev <vlaaad02@mail.ru>
2025-01-08 13:45:14 +0300

    Working with Files, Streams, and Serialization

---
+++ Chapter_9/WorkingWithFileSystems/Program.cs
@@ -0,0 +1,141 @@
+﻿using static System.Console;
+using static System.IO.Directory;
+using static System.IO.Path;
+using static System.Environment;
+
+OutputFileSystemInfo();
+// WorkWithDrives();
+// WorkWithDirectories();
+// WorkWithFiles();
+
+static void OutputFileSystemInfo()
+{
+  WriteLine("{0,-33} {1}", arg0: "Path.PathSeparator",
+    arg1: PathSeparator);
+  WriteLine("{0,-33} {1}", arg0: "Path.DirectorySeparatorChar",
+    arg1: DirectorySeparatorChar);
+  WriteLine("{0,-33} {1}", arg0: "Directory.GetCurrentDirectory()",
+    arg1: GetCurrentDirectory());
+  WriteLine("{0,-33} {1}", arg0: "Environment.CurrentDirectory",
+    arg1: CurrentDirectory);
+  WriteLine("{0,-33} {1}", arg0: "Environment.SystemDirectory",
+    arg1: SystemDirectory);
+  WriteLine("{0,-33} {1}", arg0: "Path.GetTempPath()",
+    arg1: GetTempPath());
+
+  WriteLine("GetFolderPath(SpecialFolder");
+  WriteLine("{0,-33} {1}", arg0: "  .System)",
+    arg1: GetFolderPath(SpecialFolder.System));
+  WriteLine("{0,-33} {1}", arg0: "  .ApplicationData)",
+    arg1: GetFolderPath(SpecialFolder.ApplicationData));
+  WriteLine("{0,-33} {1}", arg0: "  .MyDocuments)",
+    arg1: GetFolderPath(SpecialFolder.MyDocuments));
+  WriteLine("{0,-33} {1}", arg0: "  .Personal)",
+    arg1: GetFolderPath(SpecialFolder.Personal));
+}
+
+static void WorkWithDrives()
+{
+  WriteLine("{0,-30} | {1,-10} | {2,-7} | {3,18} | {4,18}",
+    "NAME", "TYPE", "FORMAT", "SIZE (BYTES)", "FREE SPACE");
+
+  foreach (DriveInfo drive in DriveInfo.GetDrives())
+  {
+    if (drive.IsReady)
+    {
+      WriteLine("{0,-30} | {1,-10} | {2,-7} | {3,18:N0} | {4,18:N0}",
+        drive.Name, drive.DriveType, drive.DriveFormat,
+        drive.TotalSize, drive.AvailableFreeSpace);
+    }
+    else
+    {
+      WriteLine("{0,-30} | {1,-10}", drive.Name, drive.DriveType);
+    }
+  }
+}
+
+static void WorkWithDirectories()
+{
+  // определяем путь для новой папки
+  // начиная с папки пользователя
+  string newFolder = Combine(
+    GetFolderPath(SpecialFolder.Personal),
+    "Code", "Chapter09", "NewFolder");
+
+  WriteLine($"Working with: {newFolder}");
+
+  // проверяем, существует ли он
+  WriteLine($"Does it exist? {Exists(newFolder)}");
+
+  // создать каталог
+  WriteLine("Creating it...");
+  CreateDirectory(newFolder);
+  WriteLine($"Does it exist? {Exists(newFolder)}");
+
+  Write("Confirm the directory exists, and then press ENTER: ");
+  ReadLine();
+
+  // удалить каталог
+  WriteLine("Deleting it...");
+  Delete(newFolder, recursive: true);
+  WriteLine($"Does it exist? {Exists(newFolder)}");
+}
+
+static void WorkWithFiles()
+{
+  // определяем путь к каталогу для выходных файлов
+  // начиная с папки пользователя
+  string dir = Combine(
+    GetFolderPath(SpecialFolder.Personal),
+    "Code", "Chapter09", "OutputFiles");
+
+  CreateDirectory(dir);
+
+  // определить пути к файлам
+  string textFile = Combine(dir, "Dummy.txt");
+  string backupFile = Combine(dir, "Dummy.bak");
+
+  WriteLine($"Working with: {textFile}");
+
+  // проверить, существует ли файл
+  WriteLine($"Does it exist? {File.Exists(textFile)}");
+
+  // создайте новый текстовый файл и напишите в него строку
+  StreamWriter textWriter = File.CreateText(textFile);
+  textWriter.WriteLine("Hello, C#!");
+  textWriter.Close();  // закрываем файл и освобождаем ресурсы
+  WriteLine($"Does it exist? {File.Exists(textFile)}");
+
+  // скопируйте файл и перезапишите, если он уже существует
+  File.Copy(sourceFileName: textFile,
+    destFileName: backupFile, overwrite: true);
+  WriteLine($"Does {backupFile} exist? {File.Exists(backupFile)}");
+  Write("Confirm the files exist, and then press ENTER: ");
+  ReadLine();
+
+  //удалить файл
+  File.Delete(textFile);
+  WriteLine($"Does it exist? {File.Exists(textFile)}");
+
+  // прочитать из резервной копии текстового файла
+  WriteLine($"Reading contents of {backupFile}:");
+  StreamReader textReader = File.OpenText(backupFile);
+  WriteLine(textReader.ReadToEnd());
+  textReader.Close();
+
+  // Управление путями
+  WriteLine($"Folder Name: {GetDirectoryName(textFile)}");
+  WriteLine($"File Name: {GetFileName(textFile)}");
+  WriteLine("File Name without Extension: {0}",
+    GetFileNameWithoutExtension(textFile));
+  WriteLine($"File Extension: {GetExtension(textFile)}");
+  WriteLine($"Random File Name: {GetRandomFileName()}");
+  WriteLine($"Temporary File Name: {GetTempFileName()}");
+
+  // Получение информации о файле
+  FileInfo info = new(backupFile);
+  WriteLine($"{backupFile}:");
+  WriteLine($"Contains {info.Length} bytes");
+  WriteLine($"Last accessed {info.LastAccessTime}");
+  WriteLine($"Has readonly set to {info.IsReadOnly}");
+}
---
+++ Chapter_9/WorkingWithSerialization/Program.cs
@@ -0,0 +1,118 @@
+﻿using System.Xml.Serialization; // XmlSerializer
+using Packt.Shared; // Person 
+
+using NewJson = System.Text.Json.JsonSerializer;
+
+using static System.Console;
+using static System.Environment;
+using static System.IO.Path;
+
+// создать граф объектов
+List<Person> people = new()
+{
+  new(30000M)
+  {
+    FirstName = "Alice",
+    LastName = "Smith",
+    DateOfBirth = new(1974, 3, 14)
+  },
+  new(40000M)
+  {
+    FirstName = "Bob",
+    LastName = "Jones",
+    DateOfBirth = new(1969, 11, 23)
+  },
+  new(20000M)
+  {
+    FirstName = "Charlie",
+    LastName = "Cox",
+    DateOfBirth = new(1984, 5, 4),
+    Children = new()
+    {
+      new(0M)
+      {
+        FirstName = "Sally",
+        LastName = "Cox",
+        DateOfBirth = new(2000, 7, 12)
+      }
+    }
+  }
+};
+
+// создать объект, который будет форматировать людей как XML
+XmlSerializer xs = new(people.GetType());
+
+// создать файл для записи
+string path = Combine(CurrentDirectory, "people.xml");
+
+using (FileStream stream = File.Create(path))
+{
+  // сериализовать граф объекта в поток
+  xs.Serialize(stream, people);
+}
+
+WriteLine("Written {0:N0} bytes of XML to {1}",
+  arg0: new FileInfo(path).Length,
+  arg1: path);
+
+WriteLine();
+
+// Отображение графа сериализованного объекта
+WriteLine(File.ReadAllText(path));
+
+// Десериализация с помощью XML
+
+using (FileStream xmlLoad = File.Open(path, FileMode.Open))
+{
+  // десериализовать и преобразовать граф объекта в список лиц
+  List<Person>? loadedPeople =
+    xs.Deserialize(xmlLoad) as List<Person>;
+
+  if (loadedPeople is not null)
+  {
+    foreach (Person p in loadedPeople)
+    {
+      WriteLine("{0} has {1} children.",
+        p.LastName, p.Children?.Count ?? 0);
+    }
+  }
+}
+
+// создать файл для записи
+string jsonPath = Combine(CurrentDirectory, "people.json");
+
+using (StreamWriter jsonStream = File.CreateText(jsonPath))
+{
+  // создайте объект, который будет отформатирован как JSON
+  Newtonsoft.Json.JsonSerializer jss = new();
+
+  // сериализовать граф объекта в строку
+  jss.Serialize(jsonStream, people);
+}
+
+WriteLine();
+WriteLine("Written {0:N0} bytes of JSON to: {1}",
+  arg0: new FileInfo(jsonPath).Length,
+  arg1: jsonPath);
+
+// Отображение графа сериализованного объекта
+WriteLine(File.ReadAllText(jsonPath));
+
+// Deserializing JSON using new APIs
+
+using (FileStream jsonLoad = File.Open(jsonPath, FileMode.Open))
+{
+  // Десериализация JSON с использованием новых API
+  List<Person>? loadedPeople =
+    await NewJson.DeserializeAsync(utf8Json: jsonLoad,
+      returnType: typeof(List<Person>)) as List<Person>;
+
+  if (loadedPeople is not null)
+  {
+    foreach (Person p in loadedPeople)
+    {
+      WriteLine("{0} has {1} children.",
+        p.LastName, p.Children?.Count ?? 0);
+    }
+  }
+}
